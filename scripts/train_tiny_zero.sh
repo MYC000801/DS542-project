22854bed7f2951ce6eca47bc7ba86f9c762e7f79
Zhaolin Gao <zg292@holylogin08.rc.fas.harvard.edu>
2025-02-11 21:59:19 -0500

    init

---
+++ scripts/train_tiny_zero.sh
@@ -0,0 +1,30 @@
+python3 -m verl.trainer.main_ppo \
+data.train_files=data/train.parquet \
+data.val_files=data/test.parquet \
+data.train_batch_size=256 \
+data.val_batch_size=500 \
+data.max_prompt_length=256 \
+data.max_response_length=1024 \
+actor_rollout_ref.model.path=Qwen/Qwen2.5-3B \
+actor_rollout_ref.actor.optim.lr=1e-6 \
+actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+actor_rollout_ref.actor.ppo_micro_batch_size=2 \
+actor_rollout_ref.rollout.log_prob_micro_batch_size=2 \
+actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+actor_rollout_ref.ref.log_prob_micro_batch_size=2 \
+critic.optim.lr=1e-5 \
+critic.model.path=Qwen/Qwen2.5-3B \
+critic.ppo_micro_batch_size=2 \
+algorithm.kl_ctrl.kl_coef=0.001 \
+trainer.logger=['wandb'] \
++trainer.val_before_train=False \
+trainer.default_hdfs_dir=null \
+trainer.n_gpus_per_node=2 \
+trainer.nnodes=1 \
+trainer.save_freq=50 \
+trainer.test_freq=50 \
+trainer.project_name=countdown \
+trainer.experiment_name=qwen2.5-3b-ppo \
+trainer.total_epochs=15 2>&1 | tee verl_demo.log
+
